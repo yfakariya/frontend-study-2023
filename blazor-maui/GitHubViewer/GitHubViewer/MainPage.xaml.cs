6f8f2b21ed4c03aef137d082f32acbba9d4389e4
yfakariya <yfakariya@gmail.com>
2023-03-20 19:23:47 +0900

    Initial blazor/MAUI commit

---
+++ blazor-maui/GitHubViewer/GitHubViewer/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GitHubViewer;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ blazor-maui/GitHubViewer/GitHubViewer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GitHubViewer;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
